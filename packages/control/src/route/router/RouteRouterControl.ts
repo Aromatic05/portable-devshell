7c32b9a1fe89975742fc8d1877ed9ba1b2bc2536
Aromatic <symwww@outlook.com>
2026-07-08 00:03:06 +0800

    fix(control): validate control route target kind

---
+++ packages/control/src/route/router/RouteRouterControl.ts
@@ -0,0 +1,24 @@
+import { createError, errorCodes, type JsonValue } from "@portable-devshell/shared";
+
+import type { RpcRequestEnvelope } from "../../control/rpc/ControlRpcConnection.js";
+import { RouteHandlerControl } from "../handler/RouteHandlerControl.js";
+
+export class RouteRouterControl {
+    readonly #handler: RouteHandlerControl;
+
+    constructor(handler: RouteHandlerControl) {
+        this.#handler = handler;
+    }
+
+    async route(request: RpcRequestEnvelope): Promise<JsonValue> {
+        if (request.target.kind !== "control") {
+            throw createError({
+                code: errorCodes.targetInvalid,
+                message: "Request target must be a control target.",
+                retryable: false
+            });
+        }
+
+        return await this.#handler.handle(request.method);
+    }
+}
