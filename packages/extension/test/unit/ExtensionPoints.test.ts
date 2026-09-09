d9351845fc015a22e33a94be46ae16d840fd7e99
Aromatic <symwww@outlook.com>
2026-09-09 14:13:44 +0800

    test(extension): lock domain leaf runtime exports

--- packages/extension/test/unit/ExtensionPoints.test.ts
+++ packages/extension/test/unit/ExtensionPoints.test.ts
@@ -2,7 +2,9 @@ import assert from "node:assert/strict";
 import test from "node:test";
 
 import { defineExtensionPoint } from "../../src/index.ts";
+import * as cliApi from "../../src/cli.ts";
 import { commands } from "../../src/cli.ts";
+import * as webApi from "../../src/web.ts";
 import { applications } from "../../src/web.ts";
 
 test("domain Extension Point descriptors use stable string identity", () => {
@@ -10,4 +12,9 @@ test("domain Extension Point descriptors use stable string identity", () => {
     assert.equal(applications.id, "web.applications");
     assert.notEqual(commands, defineExtensionPoint("cli.commands"));
     assert.equal(defineExtensionPoint("cli.commands").id, commands.id);
+});
+
+test("domain leaf runtime exports contain only author-facing point descriptors", () => {
+    assert.deepEqual(Object.keys(cliApi), ["commands"]);
+    assert.deepEqual(Object.keys(webApi), ["applications"]);
 });
